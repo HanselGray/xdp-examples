3e2b5c81d10d0e8e5c19f71cd35368a6845eb375
HanselGray <124652238+HanselGray@users.noreply.github.com>
2025-11-20 17:03:54 +0700

    Shared header file

---
+++ xdp_stats_kern_user.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+/* Used by BPF-prog kernel side BPF-progs and userspace programs,
+ * for sharing xdp_stats common struct and DEFINEs.
+ */
+#ifndef __XDP_STATS_KERN_USER_H
+#define __XDP_STATS_KERN_USER_H
+
+/* This is the data record stored in the map */
+struct datarec {
+    __u16 port;
+	char[12] svc_name;
+	__u32 count;
+};
+
+struct svc_rec_t
+{
+	char[12] svc_name;
+	__u32 count;
+};
+
+#ifndef XDP_ACTION_MAX
+#define XDP_ACTION_MAX (XDP_REDIRECT + 1)
+#endif
+
+#endif /* __XDP_STATS_KERN_USER_H */
