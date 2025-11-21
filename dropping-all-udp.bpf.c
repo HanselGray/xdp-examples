20605089845432b32e0cd47ffd9bb0ba1eaaabbf
HanselGray <124652238+HanselGray@users.noreply.github.com>
2025-11-21 11:00:28 +0700

    Minor fix

--- dropping-all-udp.bpf.c
+++ dropping-all-udp.bpf.c
@@ -9,27 +9,24 @@ int xdp_prog_drop_all_UDP(struct xdp_md *ctx) /* "name" visible with bpftool */
 {
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
+
     struct hdr_cursor nh;
-	u32 nh_type=0;
-    struct ethhdr *ethh;
-    struct ethhdr *ethh;
-	struct iphdr *iph;
-	struct udphdr *udph;
-	// Start pointer
-	nh.pos = data;
+    u32 nh_type = 0;
 
+    struct ethhdr *ethh;
+    struct iphdr *iph;
+    struct udphdr *udph;
 
-	// Parsing eth header
-	nh_type = parse_ethhdr(&nh, data_end, &ethh);
+    // Start pointer
+    nh.pos = data;
+
+    // Parsing eth header
+    nh_type = parse_ethhdr(&nh, data_end, &ethh);
 
     // Parsing ip/ipv6 header
-	if (nh_type == bpf_htons(ETH_P_IP))
-	{
-		nh_type = parse_iphdr(&nh, data_end, &iph);
-	}
-
-    if (eth->h_proto == htons(ETH_P_IP))
+    if (nh_type == bpf_htons(ETH_P_IP))
         nh_type = parse_iphdr(&nh, data_end, &iph);
+        
     if (nh_type == IPPROTO_UDP)
         return XDP_DROP;
     return XDP_PASS;
